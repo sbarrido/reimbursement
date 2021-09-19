eab25083298887e7fa6d83dedd238229c2aa36bb
Sam Barrido <ssbarrido@gmail.com>
2021-09-19 10:37:48 -0400

    Initalized Springboot Application

---
+++ src/test/java/sbarrido/reimbursement/ReimbursementApplicationTests.java
@@ -0,0 +1,13 @@
+package sbarrido.reimbursement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReimbursementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
