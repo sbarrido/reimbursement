6871d92d474ec8896a35dbcf907e6a439d8ad158
Sam Barrido <ssbarrido@gmail.com>
2021-09-19 16:30:55 -0400

    Restructured Project directory

---
+++ src/main/java/sbarrido/reimbursement/model/expense/OtherExp.java
@@ -0,0 +1,32 @@
+package sbarrido.model;
+import java.util.*;
+import java.awt.*;
+
+public class OtherExp extends Reimbursement {
+    private String vendor;
+    private Image receipt;
+
+    public OtherExp(Date date, String description, String vendor, Image receipt, double cost) {
+        setDate(date);
+        setDesc(description);
+        setImage(receipt);
+        setCost(cost);
+    }
+
+    public void setCost(double val) {
+        this.cost = val;
+    }
+    public void setVendor(String s) {
+        this.vendor = s;
+    }
+    public void setImage(Image receipt) {
+        this.receipt = receipt;
+    }
+
+    public String getVendor() {
+        return this.vendor;
+    }
+    public Image getImg() {
+        return this.receipt;
+    }
+}
