c9feac78d293b8dde514f0d687335f9378cbd3b7
Sam Barrido <ssbarrido@gmail.com>
2021-10-01 12:27:42 -0400

    Modified Repository Testing to initilize BEFORE each test

--- src/test/java/sbarrido/reimbursement/unit/repository/UserRepositoryTest.java
+++ src/test/java/sbarrido/reimbursement/unit/repository/UserRepositoryTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.Assertions.*;
 
