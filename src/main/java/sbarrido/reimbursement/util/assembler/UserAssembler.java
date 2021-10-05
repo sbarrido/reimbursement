ab7dd8e2cce572713183989ab5059b47fc088b70
Sam Barrido <ssbarrido@gmail.com>
2021-10-05 12:22:30 -0400

    User Assembler removed. TODO

--- src/main/java/sbarrido/reimbursement/util/assembler/UserAssembler.java
+++ src/main/java/sbarrido/reimbursement/util/assembler/UserAssembler.java
@@ -1,20 +1,21 @@
 package sbarrido.reimbursement.util.assembler;
 
-import sbarrido.reimbursement.model.user.User;
-import sbarrido.reimbursement.model.user.Role;
-import sbarrido.reimbursement.dto.model.UserPostDto;
-import sbarrido.reimbursement.dto.model.UserGetDto;
-import java.util.Set;
-import java.util.HashSet;
+// import sbarrido.reimbursement.model.user.User;
+// import sbarrido.reimbursement.model.user.Role;
+// import sbarrido.reimbursement.dto.model.UserPostDto;
+// import sbarrido.reimbursement.dto.model.UserGetDto;
+// import java.util.Set;
+// import java.util.HashSet;
 
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.hateoas.Link;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
-public class UserAssembler extends RepresentationModelAssemblerSupport<User, UserDto> { 
 
-    public UserAssembler() {
-        super(UserController.class, User.class);
-    }
-}
+// public class UserAssembler extends RepresentationModelAssemblerSupport<User, UserDto> { 
+
+//     public UserAssembler() {
+//         super(UserController.class, User.class);
+//     }
+// }
