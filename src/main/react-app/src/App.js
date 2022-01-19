284594189d014b251adca4778950c9928ccba3f7
Sam Barrido <ssbarrido@gmail.com>
2022-01-19 11:14:44 -0500

    Initializatino of React-App. Change spring root to '/api'

--- src/main/react-app/src/App.js
+++ src/main/react-app/src/App.js
@@ -1,25 +1,41 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+class App extends Component {
+  render() {
+    return (
+      <div classname="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <h1>Hello World!</h1>
+        <p>Created first React App</p>
+      </div>
+    );
+  }
 }
+
+/** Initialized React-App
+ *  Default React-app page
+ */
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
