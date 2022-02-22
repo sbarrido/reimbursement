2d25b1ddef328031639e9507b6b9dd1b1bf1053d
Sam Barrido <ssbarrido@gmail.com>
2022-02-22 13:46:10 -0500

    Testing for Modular Tables

--- src/main/react-app/src/components/GlobalNav.js
+++ src/main/react-app/src/components/GlobalNav.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {
     Nav,
-    Navbar,
     NavbarBrand,
     NavLink,
     NavItem,
--- src/main/react-app/src/routes/UsersComponent.js
+++ src/main/react-app/src/routes/UsersComponent.js
@@ -1,70 +1,80 @@
 import React from 'react'
-import axios from 'axios';
+import axios from 'axios'
 import { Spinner, Table } from 'reactstrap'
+import { connectTable } from '../util/TableUTils.js'
+import DTOTable from '../components/DTOTable.js'
 
 class UsersComponent extends React.Component {
 
     constructor(props) {
         super(props)
-        
-        this.state = {
-             userDTOs: [],
-             isLoaded: false
-        };
-    }
-
-    componentDidMount() {
-        const url = 'http://localhost:8080/api/users';
-        axios
-            .get(url)
-            .then(({ data }) => {
-                this.setState({
-                    userDTOs: data._embedded.userDtoList,
-                    isLoaded: true
-                })
-            })
-            .catch((err) => {})
 
     }
-
     render() {
-        const { isLoaded, userDTOs } = this.state;
-        if(!isLoaded) {
-            return (
-                <div>
-                    <h1>Loading...</h1>
-                    <Spinner></Spinner>
-                </div>
-            )
-        }
-        return(
-            <div>
-                <h1> LOADED! </h1>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>
-                                ID #
-                            </th>
-                            <th>
-                                Username
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {userDTOs.map((user) => {
-                    return(
-                        <tr key={ user.id }>
-                            <th> { user.id } </th>
-                            <td> { user.username } </td>
-                        </tr>
-                    )
-                })}
-                    </tbody>
-                </Table>
-            </div>
-        );
+        const MyTable = connectTable(DTOTable);
+        return <MyTable/>;
     }
+    // constructor(props) {
+    //     super(props)
+        
+    //     this.state = {
+    //          userDTOs: [],
+    //          isLoaded: false
+    //     };
+    // }
+
+    // componentDidMount() {
+    //     const url = 'http://localhost:8080/api/users';
+    //     axios
+    //         .get(url)
+    //         .then(({ data }) => {
+    //             this.setState({
+    //                 userDTOs: data._embedded.userDtoList,
+    //                 isLoaded: true
+    //             })
+    //         })
+    //         .catch((err) => {})
+
+    // }
+    
+    // render() {
+    //     const { isLoaded, userDTOs } = this.state;
+    //     if(!isLoaded) {
+    //         return (
+    //             <div>
+    //                 <h1>Loading...</h1>
+    //                 <Spinner></Spinner>
+    //             </div>
+    //         )
+    //     }
+    //     return(
+    //         <div>
+    //             <h1> LOADED! </h1>
+    //             <Table>
+    //                 <thead>
+    //                     <tr>
+    //                         <th>
+    //                             ID #
+    //                         </th>
+    //                         <th>
+    //                             Username
+    //                         </th>
+    //                     </tr>
+    //                 </thead>
+    //                 <tbody>
+    //                 {userDTOs.map((user) => {
+    //                 return(
+    //                     <tr key={ user.id }>
+    //                         <th> { user.id } </th>
+    //                         <td> { user.username } </td>
+    //                     </tr>
+    //                 )
+    //             })}
+    //                 </tbody>
+    //             </Table>
+    //         </div>
+    //     );
+    // }
 }
 
 export default UsersComponent;
