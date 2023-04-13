ad0655a524fbb2ab66b31a40a4ad6b7b76b7d20f
timchan <timchan@iu.edu>
2023-04-13 10:59:37 -0400

    delete destination

--- src/main/java/sbarrido/reimbursement/model/expense/Mileage.java
+++ src/main/java/sbarrido/reimbursement/model/expense/Mileage.java
@@ -3,10 +3,7 @@ package sbarrido.reimbursement.model.expense;
 import sbarrido.reimbursement.model.destination.Destination;
 import java.util.Date;
 
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.CascadeType;
+import javax.persistence.*;
 
 @Entity
 @DiscriminatorValue("mileage")
@@ -14,7 +11,8 @@ public class Mileage extends Reimbursement {
     public static final double _COMMUTE = 15.2;
     private static final double _RATE = 0.56;
     //(cascade = {CascadeType.ALL})
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "destination_id")
     private Destination destination;
 
     public Mileage() {}
